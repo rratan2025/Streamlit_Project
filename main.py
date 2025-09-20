257935ec5b578e394281960b268372ce1483e831
rratan2025 <ravindraratanmaurya@gmail.com>
2025-09-20 13:03:10 +0000

    Editing sample #commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("Hello, Streamlit!")
+st.write("Welcome to your first Streamlit app.")
+st.write("This is a simple example to get you started.")
+st.write("You can add more features and interactivity as you learn more about Streamlit.")
+st.write("Happy coding!")
